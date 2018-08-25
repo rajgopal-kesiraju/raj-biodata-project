bc77ba16cbc0dd2c03a8517f1db574531aa70769
RajaGopal  Kesiraju <RajaGopal.Kesiraju@fadv.com>
2018-08-25 22:40:41 +0530

    setup start script and heroku port

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 const express = require ('express');
+const hbs = require('hbs');
+const fs = require ('fs');
+
+const port = process.env.PORT || 3000;
 
 var app = express();
 
@@ -16,4 +20,4 @@ app.get('/help', (req,res) => {
 
 
 
-app.listen (3000);
+app.listen (port);
